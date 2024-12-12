cd2d5fac8ffff7ecc7e2c34f63fed40c73d79be9
ShingKwan Terry Wong <wong0373@algonquinlive.com>
2024-12-11 21:40:41 -0500

    Add the basic weather model for fetching

--- MAD9137_Final Assignment_Weather Application/Models/WeatherModel.swift
+++ MAD9137_Final Assignment_Weather Application/Models/WeatherModel.swift
@@ -4,3 +4,30 @@
 //
 //  Created by Terry Wong on 11/12/2024.
 //
+
+import Foundation
+
+struct WeatherResponse: Codable {
+    let main: MainWeather
+    let weather: [Weather]
+    let name: String
+    let dt: Int
+}
+
+struct MainWeather: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let icon: String
+}
+
+struct City: Identifiable {
+    let id = UUID()
+    let name: String
+    var temperature: Double
+    var weatherDescription: String
+    var weatherIcon: String
+    var localTime: Date
+}
